cb800c5f8527d9be5a329b2c3b0c9bb8b10550b8
Elizabeth Acheson <eacheson@dmacc.edu>
2026-09-04 16:29:10 -0500

    Homework page and research pdf

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WDV341 Homework Page</title>
+</head>
+<body>
+   <?php
+
+echo 'WDV341 Homework Page';
+
+?>
+</body>
+</html>
